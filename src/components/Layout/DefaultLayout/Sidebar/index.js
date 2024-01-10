deb7594e06c15ad076af0b5f4090607f2287f1d4
hiennguyenvan1 <hienben10@gmail.com>
2024-01-10 22:59:02 +0700

    Nguyễn Văn Hiến

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,27 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+import { useEffect, useState } from 'react';
+
+const cx = classNames.bind(styles);
+
+const tabs = ['posts', 'comments', 'albums'];
+
 function Sidebar() {
-    return <h2>Sidebar</h2>;
+    const [post, setpost] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((res) => res.json())
+            .then((res) => setpost(res));
+    }, []);
+
+    return (
+        <div className={cx('wrapper')}>
+            {tabs.map((pos1) => (
+                <button className={cx('class')}> {pos1}</button>
+            ))}
+        </div>
+    );
 }
 
 export default Sidebar;
--- src/components/Layout/HeaderOnly/index.js
+++ src/components/Layout/HeaderOnly/index.js
@@ -1,5 +1,5 @@
 import Header from '~/components/Layout/components/Header';
-
+import Footer from '../components/Footer';
 function HeaderOnly({ children }) {
     return (
         <div>
@@ -7,6 +7,7 @@ function HeaderOnly({ children }) {
             <div className="container">
                 <div className="content">{children}</div>
             </div>
+            <Footer />
         </div>
     );
 }
